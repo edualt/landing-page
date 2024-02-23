fe6e3e8d3d6a7cb3e599bf0f6542f96692bb8559
edualt <eduardosebastianhdz503@gmail.com>
2024-02-23 00:15:07 -0600

    Add home.css and update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import Navbar from '../components/Navbar';
-import HomeImage from '../assets/home-image.jpg';
+import Navbar from "../components/Navbar";
+import HomeImage from "../assets/home-image.jpg";
+
+import "../assets/css/home.css";
 
 const Home = () => {
   return (
@@ -7,14 +9,105 @@ const Home = () => {
       <Navbar />
       <div className="flex flex-col items-center justify-center p-10">
         <div className="max-w-2xl">
-          <p className="text-5xl font-bold text-center">¡Bienvenido al mundo de las manos que hablan!</p>
-          <p className="text-2xl text-center text-zinc-700 pt-4">¿Sabías que el lenguaje de señas tiene su propia gramática y estructura lingüística única?</p>
+          <p className="text-5xl font-bold text-center">
+            Bienvenido al mundo de las manos que hablan.
+          </p>
+          <p className="text-2xl text-center text-zinc-700 pt-4">
+            ¿Sabías que el lenguaje de señas tiene su propia gramática y
+            estructura lingüística única?
+          </p>
         </div>
-        <img src={HomeImage} alt="Lenguaje de señas" className="w-96 h-96 mt-10 object-contain" />
+        <img
+          src={HomeImage}
+          alt="Lenguaje de señas"
+          className="w-96 h-96 mt-10 object-contain"
+        />
 
+        <div className="max-w-2xl mt-10 mb-5">
+          <p className="text-2xl text-center text-zinc-700">
+            En este sitio web podrás aprender el lenguaje de señas mexicano
+            (LSM) de manera fácil y rápida.
+          </p>
+        </div>
+
+        <div className="d-flex align-items-center mb-3">
+          <div className="scroll"></div>
+        </div>
+
+        <div className="flex flex-col md:flex-row justify-center">
+          <div className="w-full sm:lg:w-1/2 md:w-1/3 bg-zinc-100 rounded-xl p-5 m-5">
+            <p className="text-xl font-bold text-center mb-5">
+              Días de la semana
+            </p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/5RSCf5EQscE?si=PKIHsLodYCRYailK"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+              className="w-full object-contain rounded-xl mb-5"
+            ></iframe>
+            <p className="text-lg text-center text-zinc-700">
+              Aprende los días de la semana en lenguaje de señas.
+            </p>
+          </div>
+          <div className="w-full sm:lg:w-1/2 md:w-1/3 bg-zinc-100 rounded-xl p-5 m-5">
+            <p className="text-xl font-bold text-center mb-5">Números</p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/bh5bY6axrz4?si=09EaA6xiv9lIPRQG"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+              className="w-full  object-contain rounded-xl mb-5"
+            ></iframe>
+            <p className="text-lg text-center text-zinc-700">
+              Aprende los números en lenguaje de señas.
+            </p>
+          </div>
+          <div className="w-full sm:lg:w-1/2 md:w-1/3  bg-zinc-100 rounded-xl p-5 m-5">
+            <p className="text-xl font-bold text-center mb-5">Vehiculos</p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/p0tkv-5IYRg?si=wM2euU1BmicjR5Zc"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+              className="w-full  object-contain rounded-xl mb-5"
+            ></iframe>
+            <p className="text-lg text-center text-zinc-700">
+              Aprende los vehículos en lenguaje de señas.
+            </p>
+          </div>
+        </div>
+
+        <div className="w-full sm:lg:w-2/3 bg-zinc-100 rounded-xl p-5 m-5">
+          <p className="text-xl font-bold text-center mb-5">
+            Aprende el abecedario
+          </p>
+          <iframe
+            width="100%"
+            height="640"
+            src="https://www.youtube.com/embed/44Ze_C8WTWw?si=H0Uvjbir8PGkqMLb"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            className="w-full object-contain rounded-xl mb-5"
+          ></iframe>
+          <p className="text-lg text-center text-zinc-700">
+            Aprende el abecedario en lenguaje de señas.
+          </p>
+        </div>
       </div>
     </>
   );
-}
+};
 
 export default Home;
